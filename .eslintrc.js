2aa3d2b2a00aaa8e5e11c3b68782f79ad0007299
nivenreed <nivenreed@hotmail.com>
2022-07-21 17:27:30 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true }
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    // Prettier plugin and recommended rules
+    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:@next/next/recommended'
+  ],
+  rules: {
+    // Include .prettierrc.js rules
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: [],
+        assert: 'either',
+        depth: 25
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
