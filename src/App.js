89db694987e98dc9dd5107248b787075e13d4463
nivenreed <nivenreed@hotmail.com>
2022-07-21 16:19:08 +0100

    setting up eslint

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import React from 'react';
 import Tweet from './CabinList';
 import cabinData from './data/cabin';
 
-function App(){
-
-const cabinNameList1 = cabinData.map(data => data.cabin)
-const cabinNames = [...new Set(cabinNameList1)] 
-const cabin = cabinNames.map((data, index)=> (
-  <li key={index}>
-  <button>{data} </button>
-  </li>
-  // console.log();
-  // <li key={index}>{data}  </li>
-))
-return (
-  <ul>
-    {cabin}
-  </ul>
-)
+function App() {
+        const cabinNameList1 = cabinData.map((data) => data.cabin);
+        const cabinNames = [...new Set(cabinNameList1)];
+        const cabin = cabinNames.map((data, index) => (
+                <li key={index}>
+                        <button>{data} </button>
+                </li>
+                // console.log();
+                // <li key={index}>{data}  </li>
+        ));
+        return <ul>{cabin}</ul>;
 }
-
 
 export default App;
