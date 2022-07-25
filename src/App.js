631d80082694b7664be876cdc0b4e972d920a23f
nivenreed <nivenreed@hotmail.com>
2022-07-25 11:56:22 +0100

    Cabin List Buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Tweet from './CabinList';
-import cabinData from './data/cabin';
+import CabinList from './CabinList';
+// import cabinData from './data/cabin';
 
 function App() {
-  const cabinNameList1 = cabinData.map((data) => data.cabin);
-  const cabinNames = [...new Set(cabinNameList1)];
-  const cabin = cabinNames.map((data, index) => (
-    <li key={index}>
-      <button>{data} </button>
-    </li>
-  ));
-  return <ul>{cabin}</ul>;
+  return <CabinList />;
 }
 
 export default App;
--- src/CabinList.js
+++ src/CabinList.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import cabinData from './data/cabin';
 
-function Tweet(props) {
-  return (
-    <div className="tweet">
-      <h3>Cabin Names</h3>
-      {props.cabin}
-    </div>
-  );
+function CabinList() {
+  const click = (event) => {
+    console.log(event.target.innerHTML);
+  };
+  const cabinNameList1 = cabinData.map((data) => data.cabin);
+  const cabinNames = [...new Set(cabinNameList1)];
+  const cabin = cabinNames.map((data, index) => (
+    <li key={index}>
+      <button key={index} onClick={click}>
+        {data}{' '}
+      </button>
+    </li>
+  ));
+
+  return <ul>{cabin}</ul>;
 }
-export default Tweet;
+
+export default CabinList;
