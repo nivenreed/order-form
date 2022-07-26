b6b47219da2daa897869f47732bdb85a373f8600
nivenreed <nivenreed@hotmail.com>
2022-07-26 11:20:40 +0100

    adding CabinList folder

---
+++ src/components/CabinList/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import cabinData from '../../data/cabin';
+
+function CabinList() {
+  const click = (data) => {
+    console.log(data);
+  };
+  const cabinNameList1 = cabinData.map((data) => data.cabin);
+  const cabinNames = [...new Set(cabinNameList1)];
+  const cabin = cabinNames.map((data, index) => (
+    <li className="cabinNameList" key={index}>
+      <button className="cabinListBtn" key={index} onClick={() => click(data)}>
+        {data}
+      </button>
+    </li>
+  ));
+
+  return <ul className="NameList">{cabin}</ul>;
+}
+
+export default CabinList;
