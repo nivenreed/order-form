6ed66ec443dc880da0e25db160bcaa8e99f5dfeb
nivenreed <nivenreed@hotmail.com>
2022-07-27 15:43:07 +0100

    adding props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-
 import CabinList from './components/CabinList';
+import cabinData from './data/cabin';
 
 function App() {
-  return <CabinList />;
+  const cabinNameList1 = cabinData.map((data) => data.cabin);
+  const cabinNames = [...new Set(cabinNameList1)];
+  // const cabin = cabinNames.map((data, index),
+
+  return <CabinList name={cabinNames} />;
 }
 
 export default App;
--- src/components/CabinList/index.js
+++ src/components/CabinList/index.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import cabinData from '../../data/cabin';
 
-function CabinList() {
+function CabinList({ name }) {
   const click = (data) => {
     setCabinName(data);
-    console.log(data);
   };
   const [cabinName, setCabinName] = useState('Select Cabin');
-  const cabinNameList1 = cabinData.map((data) => data.cabin);
-  const cabinNames = [...new Set(cabinNameList1)];
-  const cabin = cabinNames.map((data, index) => (
+  // const cabinNameList1 = cabinData.map((data) => data.cabin);
+  // const cabinNames = [...new Set(cabinNameList1)];
+  const cabin = name.map((data, index) => (
     <li className="cabinNameList" key={index}>
       <button
         className={data === cabinName ? 'cabinListBtnSelected' : 'cabinListBtn'}
@@ -23,8 +22,21 @@ function CabinList() {
 
   return (
     <>
-      <h1> {cabinName} </h1>
-      <ul className="NameList">{cabin}</ul>
+      <h1>{cabinName}</h1>
+      <ul className="NameList">
+        {cabin}
+        {/* <li className="cabinNameList" key={index}>
+          <button
+            className={
+              data === cabinName ? 'cabinListBtnSelected' : 'cabinListBtn'
+            }
+            key={index}
+            onClick={() => click(data)}
+          >
+            {data}
+          </button>
+        </li> */}
+      </ul>
     </>
   );
 }
