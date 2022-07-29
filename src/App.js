d395bb6adb252544aa2dbbf565b6451d0018cccb
nivenreed <nivenreed@hotmail.com>
2022-07-29 15:16:29 +0100

    changing screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import CabinList from './components/CabinList';
+import CabinSize from './components/cabinSize';
 import Invoice from './components/invoice';
 import cabinData from './data/cabin';
 
 function App() {
   const [selectedCabinName, setSelectedCabinName] = useState('');
+  const [currentScreen, setCurrentScreen] = useState('');
   const cabinNameList1 = cabinData.map((data) => data.cabin);
   const cabinNames = [...new Set(cabinNameList1)];
 
   return (
     <>
-      <CabinList
-        cabinName={cabinNames}
-        selectedCabinName={selectedCabinName}
-        setSelectedCabinName={setSelectedCabinName}
-      />
+      {currentScreen === '' && (
+        <CabinList
+          cabinName={cabinNames}
+          selectedCabinName={selectedCabinName}
+          setSelectedCabinName={setSelectedCabinName}
+          currentScreen={currentScreen}
+          setCurrentScreen={setCurrentScreen}
+        />
+      )}
+      {currentScreen === 'cabinName' && <CabinSize />}
       <Invoice selectedCabinName={selectedCabinName} />
     </>
   );
